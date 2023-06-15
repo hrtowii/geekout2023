9bf669fbc369f0f4f90085368684b996e3fa31f9
Jun Jie <junnjiee16@gmail.com>
2023-06-15 18:49:48 +0800

    chatbot inference

--- LoadingView.js
+++
@@ -1,7 +0,0 @@
-export default function LoadingView() {
-    return (
-        <View style={styles.container}>
-            <><Text>Loading.......</Text><StatusBar style="auto" /></>
-        </View>
-    );
-}
